4f3d84ba4ecaec43712aba2ec304091d43bb3bb3
Javier <javiergraciafn@gmail.com>
2022-06-29 14:24:49 +0200

    Subida

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ enumerate_prime = enumerate(animales)
 print(list(enumerate_prime))
 
 print('cxxxx')
+
+###pruebas generadores 1
+def generaPares(limite):
+    num=1
+    while num<limite:
+        yield num*2
+        num=num+1
+devuelvePares=generaPares(10)
+
+print(next(devuelvePares))
+
+print("CODIGO FALSO")
+
+print(next(devuelvePares))
+
+print("CODIGO FALSO")
+
+print(next(devuelvePares))
