c7898ca6a17f79b1e646a794e53957fbf440a5ee
Javier <javiergraciafn@gmail.com>
2022-06-29 13:53:36 +0200

    Subida

---
+++ main.py
@@ -0,0 +1,14 @@
+#PRUEBAS APRENDIZAJE PYTHON.
+
+ass = divmod(88, 99)
+print(ass)
+
+
+print('PRUEBA ENUMERATE')
+
+animales = ['nutria', 'gato', 'perro']
+
+enumerate_prime = enumerate(animales)
+
+print(list(enumerate_prime))
+
